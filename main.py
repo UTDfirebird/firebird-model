a8262ab58bfb9ad372d7d197263e130b62b469f8
Aashu Krishnan <gpstore31@gmail.com>
2025-03-07 18:58:38 -0600

    Readded default test case

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from pydantic import ValidationError
 
 app = FastAPI()
 
-'''
+
 @app.get("/")
 async def read_root():
     return {"Tweet": "This concert is fire!"}
 
-
+'''
 # http://127.0.0.1:8000/items/5?q=somequery
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
