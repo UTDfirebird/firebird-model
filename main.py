0edd6b44f936bca79d4ddb1138d9a8125ae54327
Aashu Krishnan <gpstore31@gmail.com>
2025-03-08 21:20:53 -0600

    Redirect testing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from schema import Tweets
 from pydantic import ValidationError
@@ -27,7 +28,17 @@ async def read_root():
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 '''
+@app.get("/tweets/", include_in_schema=False)
+async def redirect_tweets(request: Request):
+    scheme = request.headers.get("X-Forwarded-Proto", "https")
+    new_location = f"{scheme}://{request.headers.get('Host')}/tweets"
+    return RedirectResponse(url=new_location, status_code=307)
+
 @app.post("/tweets")
-async def create_tweets(parsedData: dict):
+async def create_tweets(parsedData: dict, request: Request):
+    print(f"X-Forwarded-Proto: {request.headers.get('X-Forwarded-Proto')}")
     return parsedData
-    #return serverModelUse.process_server_data(parsedData)
+
+@app.post("/tweets/")
+async def create_tweets_with_slash(parsedData: dict, request: Request):
+    return await create_tweets(parsedData)
